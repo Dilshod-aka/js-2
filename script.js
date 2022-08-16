c52ea6a43ed57e08603651d171ee2f63b4087f43
Dilshod-aka <dikrizo@gmail.com>
2022-08-16 23:01:13 +0500

    js-2

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// let k = +prompt('K=')
+// let n = +prompt('N=')
+
+
+
+// for (let i = 1; i <= n; i++) {
+//    console.log(k);
+// }
+
+
+// let b=30;
+
+// for (let a = 50; a > b; a--) {
+  
+//    console.log(a);
+// }
+
+
+
+// let b=30;
+
+// for (let a = 50; a > b; a--) {
+//    if (a=== 50)
+//       continue
+//    console.log(a);
+// }
+
+
+// for (let i = 10.000; i <= 100.000; i++) {
+//    if (i % 2 == 1)
+//       continue
+//    console.log();
+//    console.log(`${i/10}kg konfet ${i/10*10000}`);
+// }
+
+// for (let i = 10; i <= 20; i++) {
+//     if (i % 1 == 1)
+//        continue
+//     console.log();
+//     console.log(`${i/10}kg konfet ${i/10*10000}`);
+//  }
+
+
+
